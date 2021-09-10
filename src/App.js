0bd428ebb71638b8ad2756e9546c19e2a56f4651
dedwards13 <danieledwards130@gmail.com>
2021-09-10 10:49:34 -0500

    fix linters

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">  
-    
-          Learn React
-      
+      <header className="App-header">
+
+        Learn React
+
       </header>
     </div>
   );
