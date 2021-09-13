b73d26ad7be0377f2246362752033eb44b368143
dedwards13 <danieledwards130@gmail.com>
2021-09-13 08:58:05 -0500

    Add validation for tittle's input

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -2,8 +2,16 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import React, { useState } from 'react';
 import { createBook } from '../actions';
-import { CATEGORIES } from '../components/CategoryFilter';
-import '../stylesheets/BooksForm.css';
+
+const CATEGORIES = [
+  { key: 0, name: 'Action' },
+  { key: 1, name: 'Biography' },
+  { key: 2, name: 'History' },
+  { key: 3, name: 'Horror' },
+  { key: 4, name: 'Kids' },
+  { key: 5, name: 'Learning' },
+  { key: 6, name: 'Sci-Fi' },
+];
 
 const BooksForm = ({ create }) => {
   const [book, setBook] = useState({ title: '', category: '' });
@@ -26,27 +34,32 @@ const BooksForm = ({ create }) => {
   };
 
   return (
-    <form className="row g-3 form-book">
-      <h1 className="add-book col-12">ADD NEW BOOK</h1>
-      <div className="col-md-6">
-        <input
-          type="text"
-          placeholder="Enter the book title"
-          onChange={handleChange}
-          value={book.title}
-          className={`form-control ${book.invalid ? 'is-invalid' : ''}`}
-          id="inputTitle"
-        />
+    <form className="row g-3">
+      <div className="col-md-4">
+        <label htmlFor="inputTitle" className="form-label">
+          Title
+          <input
+            type="text"
+            placeHolder="Enter the book title"
+            onChange={handleChange}
+            value={book.title}
+            className={`form-control ${book.invalid ? 'is-invalid' : ''}`}
+            id="inputTitle"
+          />
+        </label>
       </div>
-      <div className="col-md-3">
-        <select name="category" onChange={handleChange} className="form-select" id="category">
-          {
-            CATEGORIES.map(({ key, name }) => <option value={name} key={key}>{name}</option>)
-          }
-        </select>
+      <div className="col-md-4">
+        <label htmlFor="category" className="form-label">
+          Category
+          <select name="category" onChange={handleChange} className="form-select" id="category">
+            {
+              CATEGORIES.map(({ key, name }) => <option value={name} key={key}>{name}</option>)
+            }
+          </select>
+        </label>
       </div>
-      <div className="col-md-1">
-        <button type="button" onClick={handleSubmit} className="btn btn-create">Create book</button>
+      <div className="col-md-4">
+        <button type="button" onClick={handleSubmit} className="btn btn-primary">Create book</button>
       </div>
     </form>
   );
