c81e3d05399c4bbb31e57004b04f6a0a202176d5
dedwards13 <danieledwards130@gmail.com>
2021-09-14 15:24:46 -0500

    Fix styles of navbar

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './index.css';
 import App from './components/App';
+import NavBar from './components/NavBar';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
-
-const wrappedApp = (
-  <Provider store={store}>
-    <App />
-  </Provider>
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
-
 ReactDOM.render(
-  wrappedApp,
+  <Provider store={store}>
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route path="/" component={App} />
+        <Route path="/books" component={App} />
+        <Route path="/categories" component={App} />
+      </Switch>
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
