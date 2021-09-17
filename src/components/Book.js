e0d4ab242e38fd62fc5acb8327b19d8511f8dfca
dedwards13 <danieledwards130@gmail.com>
2021-09-16 20:24:13 -0500

    fix styles in classes of book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,34 +3,48 @@ import PropTypes from 'prop-types';
 import '../stylesheets/Book.css';
 
 const Book = ({ book, delete: handleRemoveBook }) => (
-  <div className={`${book.visible ? 'book-card flex align-c' : 'd-none'}`}>
-    <div className="book-info">
-      <div className="book-category">
-        {book.category}
+  <li className={`${book.visible ? 'book-card flex align-c' : 'd-none'}`}>
+    <div>
+      <div className="main-info">
+        <div>
+          <h4 className="book-cat op-5">{book.category}</h4>
+        </div>
+        <div>
+          <h3 className="book-title">{book.title}</h3>
+        </div>
+        <div className="book-id fw-300">
+          {`Book ID: ${book.id}`}
+        </div>
       </div>
-      <div className="book-title">{book.title}</div>
-      <div className="book-id">
-        {`Book ID: ${book.id}`}
-      </div>
-      <div className="actions">
-        <span className="first">Comments</span>
-        <a href="/#" onClick={() => handleRemoveBook(book)}>Remove</a>
-        <span className="last">Edit</span>
+
+      <div className="lower-btns fw-300 flex align-c">
+        <p className="pointer">Comments</p>
+        <div className="btn-divider" />
+        <button className="rmv-btn fw-300 pointer" type="button"
+          onClick={() => handleRemoveBook(book)}>Remove</button>
+        <div className="btn-divider" />
+        <p className="pointer">Edit</p>
       </div>
     </div>
-    <div className="book-info position-relative">
-      <i className="Oval-2 d-none d-md-flex" />
-      <span className="percentage">64%</span>
-      <span className="completed">Completed</span>
+
+      <div className="progress flex align-c">
+        <i className="porcentage-oval flex align-c"/>
+        <div className="porcentage-cont">
+          <p className="porcentage">64%</p>
+          <p className="completed op-5">Completed</p>
+      </div>
     </div>
-    <div className="col d-flex flex-column">
-      <span className="completed">CURRENT CHAPTER</span>
-      <span className="chapter">Chapter 17</span>
-      <button type="button" className="btn btn-update mt-3" onClick={() => handleRemoveBook(book)}>
+
+    <div className="divider" />
+
+    <div className="chapter-cont">
+      <p className="current-ch fw-300 op-5">CURRENT CHAPTER</p>
+      <p className="chapter fw-300">Chapter 17</p>
+      <button type="button" className="updt-progress fw-300 pointer" onClick={() => handleRemoveBook(book)}>
         Update Progress
       </button>
     </div>
-  </div>
+  </li>
 );
 
 Book.propTypes = {
